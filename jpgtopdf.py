4e8b949c27d43e91392ac112b49cfdb83d16192e
Nelson Parra <nparra@rgida.com>
2022-05-31 10:58:05 -0400

    origin code

---
+++ jpgtopdf.py
@@ -0,0 +1,39 @@
+from fpdf import FPDF
+import os
+
+img_path = "/Users/nelsonparra/Desktop/05232022_FE_A_RGI_2"
+pdf_path = "/Users/nelsonparra/Desktop/pdffolder"
+image = img_path
+
+# creating pdf
+pdf = FPDF(orientation="P", unit="mm", format="letter")
+images = []
+for filename in os.listdir(img_path):
+    images.append(filename)
+for jpg in images:
+    if jpg.endswith(".jpg"):
+        steven = images.index(jpg)
+        pdf.add_page()
+        pdf.image("/Users/nelsonparra/Desktop/05232022_FE_A_RGI_2/"+jpg,h=50,w=150)
+    else:
+        print("did not work")
+pdf.output("convertedImages.pdf")
+
+
+
+
+
+
+# for filename in os.listdir(img_path):
+#         pdf.add_page()
+#         print(filename)
+#         pdf.image(filename)
+        # pdf.image(filename)
+        # pdf.output(filename+".pdf")
+        # print("we made it")
+        # print("Successfully made pdf file")
+        # put file in folder
+        # file = open(pdf_path, "wb")
+        # file.write(pdf)
+        # file.close()
+        # print("Successfully store pdf")
